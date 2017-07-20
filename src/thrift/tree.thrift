cfaed1e6364dedcc173de04c6015c5bd4b11f7b2
David Pierce <dpierce@actioniq.co>
2017-07-20 10:42:16 -0400

    got thrift python working

--- src/thrift/tree.thrift
+++ src/thrift/tree.thrift
@@ -1,13 +1,14 @@
+namespace java tree
+namespace py tree
+
 union Tree {
   1: Leaf leaf
   2: Branch branch
 }
-
 struct Leaf {
   1: string data
 }
-
 struct Branch {
-  1: optional Tree left
-  2: optional Tree right
+  1: Tree left
+  2: Tree right
 }
